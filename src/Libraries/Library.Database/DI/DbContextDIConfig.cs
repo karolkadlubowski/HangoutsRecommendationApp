32119e6c44a71e8de71fbf1e1c2962d80fc80d09
tomeq <tomeknowok@gmail.com>
2022-04-09 01:59:09 +0200

    Add Library.Database

---
+++ src/Libraries/Library.Database/DI/DbContextDIConfig.cs
@@ -0,0 +1,12 @@
+﻿using Library.Database.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Library.Database.DI
+{
+    public static class DbContextDIConfig
+    {
+        public static IServiceCollection AddDbContext<TContext>(this IServiceCollection services)
+            where TContext : class, IDbContext
+            => services.AddScoped<IDbContext, TContext>();
+    }
+}
--- src/Libraries/Library.Database/DI/TransactionManagerDIConfig.cs
+++ src/Libraries/Library.Database/DI/TransactionManagerDIConfig.cs
@@ -7,6 +7,6 @@ namespace Library.Database.DI
     public static class TransactionManagerDIConfig
     {
         public static IServiceCollection AddTransactionManager(this IServiceCollection services)
-            => services.AddTransient<ITransactionManager, TransactionManager>();
+            => services.AddScoped<ITransactionManager, TransactionManager>();
     }
 }
