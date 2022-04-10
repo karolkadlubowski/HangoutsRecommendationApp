67a073069ba321be175c80d1e2999be2d17a37d6
karolkadlubowski <56251060+karolkadlubowski@users.noreply.github.com>
2022-04-10 16:45:52 +0200

    AccountDefinition.API: Add AccountProviderRepository.cs

--- src/Services/AccountDefinition.API/src/AccountDefinition.API.Application/Database/Repositories/IAccountProviderRepository.cs
+++ src/Services/AccountDefinition.API/src/AccountDefinition.API.Application/Database/Repositories/IAccountProviderRepository.cs
@@ -1,9 +1,12 @@
-﻿using AccountDefinition.API.Domain.Entities;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AccountDefinition.API.Domain.Entities;
 using Library.Database.Abstractions;
 
 namespace AccountDefinition.API.Application.Database.Repositories
 {
     public interface IAccountProviderRepository : IDbRepository<AccountProvider>
     {
+        Task<IReadOnlyList<AccountProvider>> GetAccountProvidersAsync();
     }
 }
---
+++ src/Services/AccountDefinition.API/src/AccountDefinition.API.Infrastructure/Database/Repositories/AccountProviderRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AccountDefinition.API.Application.Database.Queries;
+using AccountDefinition.API.Application.Database.Repositories;
+using AccountDefinition.API.Domain.Entities;
+using Library.Database.Abstractions;
+using Library.Shared.Resources;
+
+namespace AccountDefinition.API.Infrastructure.Database.Repositories
+{
+    public class AccountProviderRepository : IAccountProviderRepository
+    {
+        private readonly IDbContext _dbContext;
+        private readonly IResourceReader _resourceReader;
+
+        public AccountProviderRepository(IDbContext dbContext, IResourceReader resourceReader)
+        {
+            _dbContext = dbContext;
+            _resourceReader = resourceReader;
+        }
+        
+        public async Task<IReadOnlyList<AccountProvider>> GetAccountProvidersAsync()
+        {
+            var query = await _resourceReader.ReadResourceAsync(
+                QueryLocationFactory.PrepareQueryLocation(QueriesNames.SelectAccountProviders),
+                QueryLocationFactory.QueriesAssembly
+            );
+
+            return (await _dbContext.QueryAsync<AccountProvider>(query))
+                .ToList();
+        }
+    }
+}
