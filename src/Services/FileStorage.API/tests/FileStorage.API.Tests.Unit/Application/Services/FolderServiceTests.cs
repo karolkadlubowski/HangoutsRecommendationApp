86c7e72d29d9057b97930b079b0a768f985479b0
tomeq <tomeknowok@gmail.com>
2022-04-16 17:22:37 +0200

    FileStorage.API: Unit tests for FileService

---
+++ src/Services/FileStorage.API/tests/FileStorage.API.Tests.Unit/Application/Services/FolderServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using FileStorage.API.Application.Database.Repositories;
+using FileStorage.API.Application.Services;
+using Library.Shared.Logging;
+using Moq;
+using NUnit.Framework;
+using SimpleFileSystem.Abstractions;
+
+namespace FileStorage.API.Tests.Unit.Application.Services
+{
+    [TestFixture]
+    public class FolderServiceTests
+    {
+        private Mock<IFolderRepository> _folderRepository;
+        private Mock<IFileSystemConfiguration> _fileSystemConfiguration;
+        private Mock<IMapper> _mapper;
+        private Mock<ILogger> _logger;
+
+        private const string Key = nameof(Key);
+
+        private FolderService _folderService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _folderRepository = new Mock<IFolderRepository>();
+            _fileSystemConfiguration = new Mock<IFileSystemConfiguration>();
+            _mapper = new Mock<IMapper>();
+            _logger = new Mock<ILogger>();
+
+            _folderService = new FolderService(
+                _folderRepository.Object,
+                _fileSystemConfiguration.Object,
+                _mapper.Object,
+                _logger.Object);
+        }
+    }
+}
---
+++ src/Services/FileStorage.API/tests/FileStorage.API.Tests.Unit/Utilities/Factories/FormFileFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace FileStorage.API.Tests.Unit.Utilities.Factories
+{
+    public static class FormFileFactory
+    {
+        public static IFormFile CreateFormFileWithName(string name)
+            => new FormFile(Stream.Null, default, default, name, name);
+    }
+}
