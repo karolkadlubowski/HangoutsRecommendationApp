f57ad0f69ddd5722e2e070a654a211479754edfe
tomeq <tomeknowok@gmail.com>
2022-04-16 17:55:30 +0200

    FileStorage.API: Write unit tests for Delete handlers

--- src/Services/FileStorage.API/tests/FileStorage.API.Tests.Unit/Utilities/Models/TestFile.cs
+++ src/Services/FileStorage.API/tests/FileStorage.API.Tests.Unit/Utilities/Models/TestFile.cs
@@ -1,10 +1,15 @@
 ﻿using FileStorage.API.Domain.Entities;
+using FileStorage.API.Domain.ValueObjects;
 
 namespace FileStorage.API.Tests.Unit.Utilities.Models
 {
     public class TestFile : File
     {
-        public TestFile(string key) => Key = key;
+        public TestFile(string key)
+        {
+            FileId = new GuidIdentifier();
+            Key = key;
+        }
 
         public void SetName(string name) => Name = name;
     }
--- src/Services/FileStorage.API/tests/FileStorage.API.Tests.Unit/Utilities/Models/TestFolder.cs
+++ src/Services/FileStorage.API/tests/FileStorage.API.Tests.Unit/Utilities/Models/TestFolder.cs
@@ -1,9 +1,14 @@
 ﻿using FileStorage.API.Domain.Entities;
+using FileStorage.API.Domain.ValueObjects;
 
 namespace FileStorage.API.Tests.Unit.Utilities.Models
 {
     public class TestFolder : Folder
     {
-        public TestFolder(string key) => Key = key;
+        public TestFolder(string key)
+        {
+            FolderId = new GuidIdentifier();
+            Key = key;
+        }
     }
 }
