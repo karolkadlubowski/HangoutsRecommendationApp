e0d5d3f5e376a33bb464e07cff96de636c8494ab
karolkadlubowski <karol.kadlubowski@infotower.pl>
2022-06-01 00:02:23 +0200

    Service and Features tests added

---
+++ src/Services/VenueReview.API/tests/VenueReview.API.Tests.Unit/Application/Features/DeleteVenueReview/DeleteVenueReviewCommandHandlerTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Library.EventBus;
+using Library.Shared.Events.Abstractions;
+using Library.Shared.Models.VenueReview.Events;
+using Moq;
+using NUnit.Framework;
+using VenueReview.API.Application.Abstractions;
+using VenueReview.API.Application.Features.DeleteVenueReview;
+
+namespace VenueReview.API.Tests.Unit.Application.Features.DeleteVenueReview
+{
+    [TestFixture]
+    public class DeleteVenueReviewCommandHandlerTests
+    {
+        private Mock<IVenueReviewService> _venueReviewService;
+        private Mock<IEventSender> _eventSender;
+
+        private DeleteVenueReviewCommandHandler _deleteVenueReviewCommandHandler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _venueReviewService = new Mock<IVenueReviewService>();
+            _eventSender = new Mock<IEventSender>();
+
+            _deleteVenueReviewCommandHandler = new DeleteVenueReviewCommandHandler(_venueReviewService.Object,
+                _eventSender.Object);
+        }
+
+        [Test]
+        public async Task Handle_WhenCalled_ReturnVenueReviewCategoryResponse()
+        {
+            //Arrange
+            var venueReviewId = Guid.NewGuid().ToString();
+
+            var command = new DeleteVenueReviewCommand {VenueReviewId = venueReviewId};
+
+            _venueReviewService.Setup(x => x.DeleteVenueReviewAsync(command))
+                .ReturnsAsync(venueReviewId);
+
+            var expectedResponse = new DeleteVenueReviewResponse {DeletedVenueReviewId = venueReviewId};
+
+            //Act
+            var result = await _deleteVenueReviewCommandHandler.Handle(command, default);
+
+            //Assert
+            result.Should().BeEquivalentTo(expectedResponse);
+        }
+
+        [Test]
+        public async Task Handle_WhenCalled_ShouldInvokeProperMethodsOnce()
+        {
+            //Arrange
+            var venueReviewId = Guid.NewGuid().ToString();
+
+            var command = new DeleteVenueReviewCommand {VenueReviewId = venueReviewId};
+
+            _venueReviewService.Setup(x => x.DeleteVenueReviewAsync(command))
+                .ReturnsAsync(venueReviewId);
+
+            //Act
+            await _deleteVenueReviewCommandHandler.Handle(command, default);
+
+            //Assert
+            _venueReviewService.Verify(x => x.DeleteVenueReviewAsync(command), Times.Once);
+            _eventSender.Verify(x => x.SendEventAsync(EventBusTopics.VenueReview,
+                It.IsNotNull<VenueReviewDeletedEvent>(),
+                default), Times.Once);
+        }
+    }
+}
---
+++ src/Services/VenueReview.API/tests/VenueReview.API.Tests.Unit/Utilities/Models/StubVenueReview.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VenueReview.API.Tests.Unit.Utilities.Models
+{
+    public class StubVenueReview : API.Domain.Entities.VenueReview
+    {
+        public StubVenueReview(
+            string venueReviewId,
+            long venueId,
+            string content,
+            long creatorId,
+            double rating,
+            DateTime createdOn
+        )
+        {
+            VenueReviewId = venueReviewId;
+            VenueId = venueId;
+            Content = content;
+            CreatorId = creatorId;
+            Rating = rating;
+            CreatedOn = createdOn;
+        }
+    }
+}
