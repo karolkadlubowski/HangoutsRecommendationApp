143c3daa414b91c38e7b50c3cce3fe61633f52a7
filippyziak <dojczkrol@gmail.com>
2022-06-06 18:07:34 +0200

    Add AddUser repository method

--- src/Services/Identity.API/src/Identity.API.Application/Database/Repositories/IIdentityRepository.cs
+++ src/Services/Identity.API/src/Identity.API.Application/Database/Repositories/IIdentityRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace Identity.API.Application.Database.Repositories
+﻿using System.Threading.Tasks;
+using Identity.API.Application.Database.PersistenceModels;
+
+namespace Identity.API.Application.Database.Repositories
 {
     public interface IIdentityRepository
     {
-        
+        Task<bool> AddUserAsync(UserPersistenceModel user);
     }
 }
--- src/Services/Identity.API/src/Identity.API.Infrastructure/Database/Repositories/IdentityRepository.cs
+++ src/Services/Identity.API/src/Identity.API.Infrastructure/Database/Repositories/IdentityRepository.cs
@@ -1,9 +1,22 @@
-﻿using Identity.API.Application.Database.Repositories;
+﻿using System.Threading.Tasks;
+using Identity.API.Application.Database.PersistenceModels;
+using Identity.API.Application.Database.Repositories;
 
 namespace Identity.API.Infrastructure.Database.Repositories
 {
     public class IdentityRepository : IIdentityRepository
     {
-        
+        private readonly IdentityDbContext _identityDbContext;
+
+        public IdentityRepository(IdentityDbContext identityDbContext)
+        {
+            _identityDbContext = identityDbContext;
+        }
+
+        public async Task<bool> AddUserAsync(UserPersistenceModel user)
+        {
+            await _identityDbContext.Users.AddAsync(user);
+            return await _identityDbContext.SaveChangesAsync() > 0;
+        }
     }
 }
