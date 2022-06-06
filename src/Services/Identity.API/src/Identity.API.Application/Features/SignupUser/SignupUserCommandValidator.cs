c9330f1860dcb2917c6a8fd0cb556711d94bf148
filippyziak <dojczkrol@gmail.com>
2022-06-06 18:09:19 +0200

    Add Validation for signupUserCommand

---
+++ src/Services/Identity.API/src/Identity.API.Application/Features/SignupUser/SignupUserCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Identity.API.Domain.Validation;
+
+namespace Identity.API.Application.Features.SignupUser
+{
+    public class SignupUserCommandValidator : AbstractValidator<SignupUserCommand>
+    {
+        public SignupUserCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .EmailAddress()
+                .NotNull();
+            RuleFor(x => x.Password)
+                .NotNull()
+                .Length(ValidationRules.MinPasswordLength, ValidationRules.MaxPasswordLength);
+        }
+    }
+}
--- src/Services/Identity.API/src/Identity.API.Domain/Validation/ValidationRules.cs
+++ src/Services/Identity.API/src/Identity.API.Domain/Validation/ValidationRules.cs
@@ -2,5 +2,9 @@
 {
     public static class ValidationRules
     {
+        public const int MinPasswordLength = 4;
+        public const int MaxPasswordLength = 64;
+
+        public const int MaxEmailAddress = 255;
     }
 }
