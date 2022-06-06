9957f322c4554ec0a47be1ff6b4daf355e588d22
filippyziak <dojczkrol@gmail.com>
2022-06-06 18:08:03 +0200

    Add User entity and UserEmail ValueObject

---
+++ src/Services/Identity.API/src/Identity.API.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using Identity.API.Domain.ValueObjects;
+using Library.Shared.Models;
+
+namespace Identity.API.Domain.Entities
+{
+    public class User : RootEntity
+    {
+        public long UserId { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public User(string email, byte[] passwordSalt, byte[] passwordHash)
+        {
+            Email = new UserEmail(email);
+            PasswordSalt = passwordSalt;
+            PasswordHash = passwordHash;
+        }
+    }
+}
---
+++ src/Services/Identity.API/src/Identity.API.Domain/ValueObjects/UserEmail.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using Identity.API.Domain.Validation;
+using Library.Shared.Exceptions;
+using Library.Shared.Models;
+using ValidationException = Library.Shared.Exceptions.ValidationException;
+
+namespace Identity.API.Domain.ValueObjects
+{
+    public record UserEmail : ValueObject<string>
+    {
+        public UserEmail(string email)
+            => Value = email != null && new EmailAddressAttribute().IsValid(email)
+                ? email.Length > ValidationRules.MaxEmailAddress
+                    ? email
+                    : throw new ValidationException($"{nameof(email)} cannot exceed {ValidationRules.MaxEmailAddress} characters")
+                : throw new ValidationException($"{nameof(email)} is not valid");
+    }
+}
