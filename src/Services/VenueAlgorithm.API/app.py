5f248fc60d18aa99e08e89ede9886dd5d04b48ba
RocketFan <kubal123.tomczak@gmail.com>
2022-06-14 23:09:37 +0200

    Example of kafka consumer

--- src/Services/VenueAlgorithm.API/app.py
+++ src/Services/VenueAlgorithm.API/app.py
@@ -1,4 +1,7 @@
+import json
+
 from flask import Flask, request, jsonify
+from kafka import KafkaConsumer
 
 app = Flask(__name__)
 
@@ -13,5 +16,23 @@ def get_venues():
 
     return res
 
+class VenueKafka:
+    def __init__(self):
+        self.venue_consumer = KafkaConsumer('venue', bootstrap_servers=['localhost:29092'],
+                                            api_version=(0,10),
+                                            auto_offset_reset='earliest',
+                                            enable_auto_commit=True,
+                                            group_id='my-group',
+                                            value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+
+    def consume(self):
+        print('Start consmue')
+
+        for message in self.venue_consumer:
+            message = message.value
+            print(message)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    venue_kafka = VenueKafka()
+    venue_kafka.consume()
