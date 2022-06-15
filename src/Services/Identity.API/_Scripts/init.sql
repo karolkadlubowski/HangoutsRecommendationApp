e29d3e6e4b1e1289bc900ab146c325e1ab92e599
filippyziak <dojczkrol@gmail.com>
2022-06-15 22:31:45 +0200

    Fix venue init script

--- src/Services/Identity.API/_Scripts/init.sql
+++ src/Services/Identity.API/_Scripts/init.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS "Identity"."Users"
     "IsDeleted" boolean DEFAULT false NOT NULL,
     "CreatedOn" timestamp without time zone DEFAULT (now() at time zone 'utc') NOT NULL,
     "ModifiedOn" timestamp without time zone
-    );
+);
 
 --INDEXES--
 CREATE INDEX ON "Identity"."Users" ("Email");
