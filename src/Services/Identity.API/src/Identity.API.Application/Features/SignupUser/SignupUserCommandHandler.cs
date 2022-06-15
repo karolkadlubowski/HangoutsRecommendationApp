246425d1f2f2943d6b96164cfe2e05cc53bafa28
filippyziak <dojczkrol@gmail.com>
2022-06-15 19:01:58 +0200

    Add event for creating user

--- src/Services/Identity.API/src/Identity.API.Application/Features/SignupUser/SignupUserCommandHandler.cs
+++ src/Services/Identity.API/src/Identity.API.Application/Features/SignupUser/SignupUserCommandHandler.cs
@@ -5,8 +5,12 @@ using Identity.API.Application.Abstractions;
 using Identity.API.Application.Database.PersistenceModels;
 using Identity.API.Application.Database.Repositories;
 using Identity.API.Domain.Entities;
+using Library.EventBus;
+using Library.Shared.Events.Abstractions;
 using Library.Shared.Exceptions;
 using Library.Shared.Logging;
+using Library.Shared.Models.Identity.Events;
+using Library.Shared.Models.Identity.Events.DataModels;
 using MediatR;
 
 namespace Identity.API.Application.Features.SignupUser
@@ -15,16 +19,19 @@ namespace Identity.API.Application.Features.SignupUser
     {
         private readonly IIdentityRepository _identityRepository;
         private readonly IPasswordHashService _passwordHashService;
+        private readonly IEventSender _eventSender;
         private readonly IMapper _mapper;
         private readonly ILogger _logger;
 
         public SignupUserCommandHandler(IIdentityRepository identityRepository,
             IPasswordHashService passwordHashService,
+            IEventSender eventSender,
             IMapper mapper,
             ILogger logger)
         {
             _identityRepository = identityRepository;
             _passwordHashService = passwordHashService;
+            _eventSender = eventSender;
             _mapper = mapper;
             _logger = logger;
         }
@@ -43,9 +50,16 @@ namespace Identity.API.Application.Features.SignupUser
 
             if (!await _identityRepository.AddUserAsync(userPersistanceModel))
                 throw new DatabaseOperationException($"Adding new user with email '{request.Email}' to database failed");
+            
+            
+            user.AddDomainEvent(EventFactory<UserCreatedEvent>.CreateEvent(user.UserId,
+                _mapper.Map<UserCreatedEventDataModel>(user)));
 
+            await _eventSender.SendEventAsync(EventBusTopics.Identity, user.FirstStoredEvent,
+                cancellationToken);
+            
             _logger.Info($"User with email '{user.Email}' created");
-
+            
             return new SignupUserResponse();
         }
     }
