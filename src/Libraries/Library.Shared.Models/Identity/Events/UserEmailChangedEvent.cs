c7b9cf6b42db4a3a2dd79fd91edfbf4329433eb8
filippyziak <dojczkrol@gmail.com>
2022-06-15 18:59:42 +0200

    Add UserCreated Event to library

--- src/Libraries/Library.Shared.Models/Identity/Events/UserEmailChangedEvent.cs
+++ src/Libraries/Library.Shared.Models/Identity/Events/UserEmailChangedEvent.cs
@@ -4,6 +4,6 @@ namespace Library.Shared.Models.Identity.Events
 {
     public record UserEmailChangedEvent : Event
     {
-        public UserEmailChangedEvent() => EventType = EventType.USER_EMAIL_CHANGED;
+        public UserEmailChangedEvent() => EventType = EventType.USER_CREATED;
     }
 }
