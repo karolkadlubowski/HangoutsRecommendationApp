20ae9825821738ba52e457b1f4bf13ceb16f920e
mikegor <michgor00@gmail.com>
2022-06-17 09:23:21 +0200

    recommendation algorithm added

--- src/Services/VenueAlgorithm.API/app.py
+++ src/Services/VenueAlgorithm.API/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from venue_kafka import VenueKafka
 from identity_kafka import IdentityKafka
 from neo4j import GraphDatabase
+from neo4j.exceptions import ServiceUnavailable
+import json
 
 app = Flask(__name__)
 
@@ -44,9 +46,35 @@ def route_update_relation():
         return res
 
 def get_venues(tx, message):
+    # data_dict = json.loads(message['Data'])
+    # query = (
+    #     "MATCH (u1:User {userId: $userId})-[:liked]->(u1Venue:Venue)<-[:liked|:saved]-(similarUser1:User)-[:liked|:saved]->(similarUserVenue1:Venue) where similarUser1 <> u1 and similarUserVenue1 <> u1Venue LIMIT 5"
+    #     "MATCH (u2:User {userId: $userId})-[:saved]->(u2Venue:Venue)<-[:liked|:saved]-(similarUser2:User)-[:liked|:saved]->(similarUserVenue2:Venue) where similarUser2 <> u2 and similarUserVenue2 <> u2Venue LIMIT 5"
+    #     "RETURN similarUserVenue1, similarUserVenue2"
+    # )
+    # result = tx.run(query, userId=data_dict['UserId'])
+    # try:
+    #     return [{"similarUserVenue1": record["similarUserVenue1"]["venueId"], "similarUserVenue2": record["similarUserVenue2"]["venueId"]}
+    #             for record in result]
+    # except ServiceUnavailable as exception:
+    #     print(exception)
+    #     raise
     pass
 
 def update_relation(tx, message):
+    # data_dict = json.loads(message['Data'])
+    # query = (
+    #     "MATCH (u1:User {userId: $userId}), (v1:Venue {venueId: $venueId})"
+    #     "CREATE (u1)-[r:liked]->(v1)"
+    #     "RETURN u1"
+    # )
+    # result = tx.run(query, userId=data_dict['UserId'], venueId=data_dict['VenueId'])
+    # try:
+    #     return [{"r": record["u1"]}
+    #             for record in result]
+    # except ServiceUnavailable as exception:
+    #     print(exception)
+    #     raise
     pass
 
 
