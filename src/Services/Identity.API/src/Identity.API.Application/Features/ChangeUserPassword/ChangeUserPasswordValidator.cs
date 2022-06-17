9b95dbdb5d1a0cf97a332f00b0b0e7e3fac35b48
tomeq <tomeknowok@gmail.com>
2022-06-17 20:59:11 +0200

    Add missing validators

---
+++ src/Services/Identity.API/src/Identity.API.Application/Features/ChangeUserPassword/ChangeUserPasswordValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Identity.API.Domain.Validation;
+
+namespace Identity.API.Application.Features.ChangeUserPassword
+{
+    public class ChangeUserPasswordValidator : AbstractValidator<ChangeUserPasswordCommand>
+    {
+        public ChangeUserPasswordValidator()
+        {
+            RuleFor(x => x.Password)
+                .NotNull()
+                .NotEmpty()
+                .Length(ValidationRules.MinPasswordLength, ValidationRules.MaxPasswordLength);
+        }
+    }
+}
